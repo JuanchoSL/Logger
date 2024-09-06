0aa3b53504395ee1ab76d9b47367745293777f63
Juan Sánchez <JuanchoSL@hotmail.com>
2024-09-06 17:45:36 +0200

    change name textcomposer instead plaintext

--- src/Composers/PlainText.php
+++ src/Composers/PlainText.php
@@ -4,24 +4,13 @@ declare(strict_types=1);
 
 namespace JuanchoSL\Logger\Composers;
 
-class PlainText extends AbstractComposer
+class PlainText extends TextComposer
 {
 
     public function compose(): mixed
     {
-        if ($this->exception instanceof \Throwable) {
-            $message = $this->exception->getCode() . ": " . $this->message . PHP_EOL;
-            $message .= "Origin: " . $this->exception->getFile() . " (" . $this->exception->getLine() . ")" . PHP_EOL;
-            $message .= $this->exception->getTraceAsString();
-        } else {
-            $message = $this->message;
-        }
-
-        $string = "[" . $this->time_mark->format($this->timeformat) . "] [" . $this->level . "] " . $message;
-        if (!empty ($this->context)) {
-            $string .= PHP_EOL . json_encode($this->context, JSON_PRETTY_PRINT);
-        }
-        return $string;
+        trigger_error("The PlainText composer is deprecated, use TextComposer instead it", E_USER_DEPRECATED);
+        return parent::compose();
     }
 
 }

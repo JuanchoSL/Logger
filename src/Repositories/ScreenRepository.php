c79329418a048d18d0450fd56060af07146c176b
Juan Sánchez <JuanchoSL@hotmail.com>
2026-01-14 13:23:38 +0100

    use formatting for screen reporitory output

--- src/Repositories/ScreenRepository.php
+++ src/Repositories/ScreenRepository.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace JuanchoSL\Logger\Repositories;
 
@@ -10,13 +8,23 @@ class ScreenRepository extends AbstractRepository
     public function save(string $level, \Stringable|string $message, array $context = []): bool
     {
         $result = $this->getComposed($level, $message, $context);
+        $data = '';
         if (is_array($result)) {
-            print_r($result);
+            $data .= print_r($result, true);
+        } elseif (is_string($result) OR $result instanceof Stringable) {
+            $data .= (string) $result;
+        } elseif ($result instanceof \JsonSerializable) {
+            $data .= (string) json_encode($result, JSON_PRETTY_PRINT);
         } elseif (is_object($result)) {
+            ob_start();
             var_dump($result);
-        } elseif (is_string($result)) {
-            echo $result;
+            $data .= ob_get_contents();
+            ob_end_clean();
         }
+        if (PHP_SAPI != 'cli') {
+            $data = "<pre>" . $data . "</pre>";
+        }
+        echo $data;
         return true;
     }
 }
